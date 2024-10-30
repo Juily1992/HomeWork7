dd2dbca2a6ccf2d8ff3e25fb4c7160b92d5e9f98
Juily1992 <juilliet1992@gmail.com>
2024-10-30 20:23:04 +0300

    Исправленные 7 и 8 задачи

--- src/Main.java
+++ src/Main.java
@@ -16,16 +16,18 @@ public class Main {
             System.out.println("Месяц " + i + " , сумма накоплений равна " + sum);
         }
         System.out.println(i + " месяцев потребуется для накопления");
-            // Task 2
+        // Task 2
         System.out.println("  ");
         System.out.println("Задание 2");
-        int a= 0;
-        while (a<10) {
+        int a = 0;
+        while (a < 10) {
             a++;
-            System.out.print(a + "  ");   }
+            System.out.print(a + "  ");
+        }
         System.out.println("  ");
-            for (a=10; a>0; a--) {
-                System.out.print( a + "  ");}
+        for (a = 10; a > 0; a--) {
+            System.out.print(a + "  ");
+        }
         System.out.println("  ");
 
         // Task 3
@@ -36,28 +38,28 @@ public class Main {
         int death = 8;
         int birth = 17;
         int year = 0;
-        int bith = (Math.abs(death-birth)*population)/1000;
-         do {
+        int bith = (Math.abs(death - birth) * population) / 1000;
+        do {
             year++;
             population = population + bith;
-            System.out.println("Год " + year + " , численность населения составляет " + population);}
-        while (year<10);
+            System.out.println("Год " + year + " , численность населения составляет " + population);
+        } while (year < 10);
 
         // Task 4
 
         System.out.println("  ");
         System.out.println("Задание 4");
         double deposit = 0;
-        int constantDeposit= 15000;
-        int month =0;
+        int constantDeposit = 15000;
+        int month = 0;
         double percent = 0.07;
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
         String result = decimalFormat.format(deposit);
-        while (deposit<11000000) {
+        while (deposit < 11000000) {
             month++;
-        deposit += deposit*percent + constantDeposit;
-            System.out.println(String.format("Месяц %d , сумма накоплений равна " + decimalFormat.format(deposit) , month));
-             }
+            deposit += deposit * percent + constantDeposit;
+            System.out.println(String.format("Месяц %d , сумма накоплений равна " + decimalFormat.format(deposit), month));
+        }
         System.out.println(month + " месяцев потребуется для накопления");
 
 
@@ -65,14 +67,14 @@ public class Main {
         System.out.println("  ");
         System.out.println("Задание 5");
         double deposit5 = 0;
-        int month5 =0;
+        int month5 = 0;
         int constantDeposit5 = 15000;
         double percent5 = 0.07;
         DecimalFormat decimalFormat5 = new DecimalFormat("#.##");
         String result5 = decimalFormat5.format(deposit5);
-        while (deposit5<11000000) {
+        while (deposit5 < 11000000) {
             month5++;
-            deposit5 += deposit5 * percent5+ constantDeposit5;
+            deposit5 += deposit5 * percent5 + constantDeposit5;
             if (month5 % 6 == 0) {
                 System.out.println(String.format("Месяц %d , сумма накоплений равна " + decimalFormat.format(deposit5), month5));
             }
@@ -85,16 +87,16 @@ public class Main {
         System.out.println("Задание 6");
         double deposit6 = 0;
         int yearM = 108;
-        int month6 =1;
+        int month6 = 1;
         double percent6 = 0.07;
         DecimalFormat decimalFormat6 = new DecimalFormat("#.##");
         String result6 = decimalFormat.format(deposit6);
-         while (month6<yearM) {
+        while (month6 < yearM) {
             month6++;
             deposit6 += deposit6 * percent6 + 15000;
-            if (month6 % 6 == 0 ) {
+            if (month6 % 6 == 0) {
                 int yearN = 1;
-                yearN= month6/12;
+                yearN = month6 / 12;
                 System.out.println(String.format("Месяц %d , %d год, сумма накоплений равна " + decimalFormat.format(deposit6), month6, yearN));
             }
         }
@@ -104,15 +106,12 @@ public class Main {
         System.out.println("  ");
         System.out.println("Задание 7");
         int day = 1;
-        int friday = 1;
-         do {
-             day++;
-             if (day % 7 == 0) {
-                 friday = friday + 1;
-                 System.out.println(String.format("Сегодня %d пятница , %d -е число. Необходимо подготовить отчет.", friday, day));
-             }
-         }
-             while (day < 31) ;
+        while (day < 31) {
+            day++;
+            if (day % 7 == 0) {
+                System.out.println(String.format("Сегодня пятница , %d -е число. Необходимо подготовить отчет.", day));
+            }
+        }
 
 
         // Task 8
@@ -122,17 +121,14 @@ public class Main {
         int yearPast = 1824;
         int yearNow = 2024;
         int yearFuture = 2124;
-        while (yearPast < yearNow) {
+        while (yearPast <= yearFuture) {
             yearPast++;
             if (yearPast % 79 == 0) {
-                System.out.println(yearPast);}
-        }
-        while (yearNow<yearFuture) {
-            yearNow++;
-            if (yearNow % 79 == 0) {
-                System.out.println(yearNow);}
+                System.out.println(yearPast);
+            }
         }
     }
 }
+
 
 
